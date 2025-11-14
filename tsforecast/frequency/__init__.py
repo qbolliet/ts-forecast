35bb01e6e8f073236baa5ba052fa0072186157ef
Quentin Bolliet <bolliet.quentin@gmail.com>
2025-11-14 19:21:09 +0100

    (refacto) remove old imports

--- tsforecast/frequency/__init__.py
+++ tsforecast/frequency/__init__.py
@@ -6,44 +6,8 @@ with support for pandas frequency codes, DateOffsets, and user-friendly frequenc
 
 # Import des classes principales
 from .detector import FrequencyDetector
-from ..utils.frequency.converter import FrequencyConverter
-from ..utils.frequency.normalizer import FrequencyNormalizer
-
-# Import des utilitaires de normalisation depuis normalizer
-from ..utils.frequency.normalizer import (
-    normalize_frequency,
-    to_literal,
-    to_code,
-    to_pandas_freq,
-    to_dateoffset,
-    is_higher_frequency,
-    validate_frequency,
-    get_frequency_order
-)
-
-# Types exportés
-from ..utils.frequency.normalizer import FrequencyType, UserFrequencyType
-from ..utils.frequency.converter import AggregationMethod, InterpolationMethod
 
 __all__ = [
     # Classes principales
     'FrequencyDetector',
-    'FrequencyConverter',
-    'FrequencyNormalizer',
-
-    # Fonctions utilitaires
-    'normalize_frequency',
-    'to_literal',
-    'to_code',
-    'to_pandas_freq',
-    'to_dateoffset',
-    'is_higher_frequency',
-    'validate_frequency',
-    'get_frequency_order',
-
-    # Types
-    'FrequencyType',
-    'UserFrequencyType',
-    'AggregationMethod',
-    'InterpolationMethod'
 ]
