2c084285d9c11a87158a032c525e0bc06e2f4ce4
Quentin Bolliet <bolliet.quentin@gmail.com>
2026-02-08 19:06:29 +0100

    (wip) p1 -> imputation rename

--- tsforecast/frequency/__init__.py
+++ tsforecast/frequency/__init__.py
@@ -10,7 +10,7 @@ from .detector import FrequencyDetector, detect_frequency, detect_dataset_freque
 from .high_frequency_imputer import HighFrequencyImputer
 from .parser import parse_frequency, build_frequency_string
 from .provenance import ImputationProvenanceTracker, ProvenanceType
-from .p1_window import P1WindowCalculator, ImputationScope
+from .imputation_window import ImputationWindowCalculator, ImputationScope
 
 __all__ = [
     # Classes et fonctions principales
@@ -26,7 +26,7 @@ __all__ = [
     'ImputationProvenanceTracker',
     'ProvenanceType',
     # P1 window calculation
-    'P1WindowCalculator',
+    'ImputationWindowCalculator',
     'ImputationScope',
     # Parser functions
     'parse_frequency',
